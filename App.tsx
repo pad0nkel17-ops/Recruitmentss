619ba6ffca627e94d6c29a499f2d31c125f1a2c3
pad0nkel17-ops <pad0nkel17@gmail.com>
2026-04-21 12:45:24 +0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,6 +1,3 @@
-import { appPromise } from '../server';
-
-export default async (req: any, res: any) => {
-  const app = await appPromise;
-  return app(req, res);
-};
+User-agent: *
+Disallow:
+# Recruiter OS Static Test
